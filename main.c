3b99e4bc189ee11b04c7a71437f2c056bac72519
lucas.schmidt015 <lucas.schmidt015@gmail.com>
2024-08-19 20:23:06 -0300

    Defining the structs.

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+//This struct is difinig how the table dictionare should looks like
+typedef struct
+{
+    int id;
+    char logical_name[20];
+    char phisical_name[20];
+} table;
+
+//This is struct is defining how the attributes dictionare should looks like
+typedef struct
+{
+    int id;
+    char name[20];
+    char type;
+    char isOptional;
+    int size;
+} att;
+
+
+
 int main(int argc, char *argv[]) {
     printf("=================Welcome=================\n");
 
